8a0a66b2c941eac659772d6ae6b775a57524869c
Allex Aldir <114629926+AllexAldir@users.noreply.github.com>
2022-12-09 22:37:11 -0300

    Create ex020.py

---
+++ condições_básicas/ex020.py
@@ -0,0 +1,10 @@
+# 20) Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou
+#     ÍMPAR.
+
+num = int(input("Digite um número para saber se é par ou é ímpar: "))
+
+if num %2 == 0:
+    print("Número é par!")
+else:
+    print("Número ímpar!")
+    
